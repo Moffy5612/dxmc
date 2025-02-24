e6c75b1c15b83e87e9f8c93952575e65fff390ae
Moffy5612 <a-eihave@outlook.jp>
2025-02-24 11:25:41 +0900

    #

--- cc/applied_energistics2/monitor.lua
+++ cc/applied_energistics2/monitor.lua
@@ -3,9 +3,17 @@ local meBridge = peripheral.wrap("bottom")
 local duration = 1
 local relayComputerId = 1
 
+function to_serial_list(tbl)
+    local list = {}
+    for _, v in pairs(tbl) do
+        list [#list + 1] = textutils.serialiseJSON(tbl)
+    end
+end
+
 while true do
+
     local d = {
-        items = meBridge.listItems()
+        items = to_serial_list(meBridge.listItems())
     }
 
     for k, v in pairs(d) do
